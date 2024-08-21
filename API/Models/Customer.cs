420b5e8d0f8cb15b5366938ffc39c3a10b0b773b
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-08-21 10:36:24 +0200

    Update

---
+++ API/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace HotelBooking.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        public ICollection<Order> Orders { get; set; } = null!;
+    }
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+
+    }
+}
