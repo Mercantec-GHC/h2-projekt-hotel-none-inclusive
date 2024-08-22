5a967692bc28d3bb504a9f831e8dda57146bf3ab
MdotKvist <Kvist339@gmail.com>
2024-08-22 10:32:06 +0200

    Made the form work

--- API/Models/SignUp.cs
+++ API/Models/SignUp.cs
@@ -13,10 +13,21 @@ namespace API.Models
             _context = context;
         }
 
-        public void SignUpRegistreUser()
+        // Method to register a user with dynamic parameters
+        public void SignUpRegistreUser(string firstName, string lastName, string email, string address, string city, string country, string zip)
         {
             try
             {
+                // Set user properties
+                this.FirstName = firstName;
+                this.LastName = lastName;
+                this.Email = email;
+                this.Address = address;
+                this.City = city;
+                this.Country = country;
+                this.Zip = zip;
+
+
                 // Adding new user - This to use DbSet
                 _context.Users.Add(this);
 
