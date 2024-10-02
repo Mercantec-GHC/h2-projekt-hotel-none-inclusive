7a985e955fcd8ca75c92bde4437359dd695b59e1
Sckaletz <allan@leeloo.dk>
2024-10-02 14:08:10 +0200

    Fixed refresh bug

--- React/hotel-booking-project - admin/src/App.jsx
+++ React/hotel-booking-project - admin/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         if (!isAuthenticated) {
             navigate('/login');
         }
-    })
+    }, [isAuthenticated, navigate]);
 
     return (
         <>
--- React/hotel-booking-project - admin/src/context/AuthContext.jsx
+++ React/hotel-booking-project - admin/src/context/AuthContext.jsx
@@ -1,14 +1,27 @@
-import { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    const login = () => setIsAuthenticated(true);
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            setIsAuthenticated(true);
+        }
+    }, []);
+
+    const login = (token) => {
+        localStorage.setItem('token', token);
+        setIsAuthenticated(true);
+    };
+
     const logout = () => {
-        setIsAuthenticated(false);
         localStorage.removeItem('token');
+        setIsAuthenticated(false);
     };
 
     return (
@@ -17,5 +30,3 @@ export const AuthProvider = ({ children }) => {
         </AuthContext.Provider>
     );
 };
-
-export const useAuth = () => useContext(AuthContext);
