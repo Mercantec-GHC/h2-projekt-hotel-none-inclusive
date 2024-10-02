882dde0de71e3bb41aaddd5f7b0c9544be52ce77
Sckaletz <allan@leeloo.dk>
2024-10-02 14:01:20 +0200

    Fixed refresh

--- React/hotel-booking-project - admin/src/App.jsx
+++ React/hotel-booking-project - admin/src/App.jsx
@@ -1,21 +1,19 @@
-import {Route, Routes, useNavigate} from "react-router-dom";
-import './App.css'
+import { Route, Routes, useNavigate } from "react-router-dom";
+import './App.css';
 import Home from "./pages/Home/Home.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import LoginPage from "./pages/Login & Signup/LoginPage.jsx"
+import LoginPage from "./pages/Login & Signup/LoginPage.jsx";
 import SignupPage from "./pages/Login & Signup/SignupPage.jsx";
 import Rooms from "./pages/Rooms/Rooms.jsx";
-import BookingsPage  from "./pages/BookingsPage/BookingsPage.jsx";
+import BookingsPage from "./pages/BookingsPage/BookingsPage.jsx";
 import Contact from "./pages/Contact/Contact.jsx";
 import UsersPage from "./pages/Users/UsersPage.jsx";
-import {useEffect} from "react";
-import {useAuth} from "./context/AuthContext.jsx";
+import { useEffect } from "react";
+import { useAuth } from "./context/AuthContext.jsx";
 import CreateRoomPage from "./pages/CreateRoom/CreateRoomPage.jsx";
 
-
 function App() {
-
     const navigate = useNavigate();
     const { isAuthenticated } = useAuth();
 
@@ -23,13 +21,12 @@ function App() {
         if (!isAuthenticated) {
             navigate('/login');
         }
-    })
+    }, [isAuthenticated, navigate]);
 
     return (
         <>
             <div className="container">
                 <Navbar />
-
                 <div className="page-container">
                     <Routes>
                         <Route path="/" element={<Home />} />
@@ -42,11 +39,10 @@ function App() {
                         <Route path="/create-room" element={<CreateRoomPage />} />
                     </Routes>
                 </div>
-
                 <Footer />
             </div>
         </>
-    )
+    );
 }
 
-export default App
+export default App;
